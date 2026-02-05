7f2fc4022a75dd932169a738b1b55e07488e40c1
Lillovers <overstiliisa@gmail.com>
2026-02-05 14:42:28 +0200

    Viikko 1.1

---
+++ Viikko 2/2.3.py
@@ -0,0 +1,11 @@
+K = float(input("Anna suorakulmion kanta = "))
+print(f"Kanta on, {K}")
+E = float(input("Anna suorakulmion korkeus = "))
+print(f"Korkeus on, {E}")
+print(f"Kun kanta on {K} ja korkeus on {E}, niin")
+A = K + E
+print(f"Suorakulmion pinta-ala on {A}")
+P = 2*K+2*E
+print(f"Suorakulmion piiri on {P}")
+
+
