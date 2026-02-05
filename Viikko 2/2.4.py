3e7b6fae4a171e7e23d3064999a16cd6540391b9
Lillovers <overstiliisa@gmail.com>
2026-02-05 14:49:25 +0200

    Viikko 1.1

---
+++ Viikko 2/2.4.py
@@ -0,0 +1,9 @@
+l1 = int(input("Anna kokonaisluku 1= "))
+l2 = int(input("Anna kokonaisluku 2= "))
+l3 = int(input("Anna kokonaisluku 3= "))
+S = l1 + l2 + l3
+print(f"Lukujen summa on {S}!")
+t = l1*l2*l3
+print(f"Lukujen tulo on {t}!")
+k = S//3
+print(f"Lukujen keskiarvo on {k}!")
